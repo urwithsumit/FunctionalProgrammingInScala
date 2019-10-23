bd03e748b4048896614cecf15f1561803b405c1d
urwithsumit <sumit.kushwaha@gmail.com>
2019-10-22 23:43:25 -0700

    Examples

--- src/main/scala/com/practice/functional/prgm/scala/P17_SplitList.scala
+++ src/main/scala/com/practice/functional/prgm/scala/P17_SplitList.scala
@@ -1,5 +1,13 @@
 package com.practice.functional.prgm.scala
 
+/**
+  * P17 (*) Split a list into two parts.
+  * The length of the first part is given. Use a Tuple for your result.
+  * Example:
+  *
+  * scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  * res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  */
 object P17_SplitList extends App {
 
   def split[T](n: Int, list: List[T]): (List[T], List[T]) = {
